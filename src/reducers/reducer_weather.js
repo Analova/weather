37d56a3b1e0da47d5fa75958a1ca00a8eecfce7a
Ana Nikolova <ani.nik19@gmail.com>
2019-09-29 10:18:23 +0200

    can search and display the name of the city

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,10 @@
 import { FETCH_WEATHER } from "../actions/index";
 
 export default function(state = [], action) {
+  if (action.error) {
+    alert("City not found! Check city spelling.");
+    return state;
+  }
   switch (action.type) {
     case FETCH_WEATHER:
       //return state.concat([action.payload.data]);
